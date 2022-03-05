ae2567693ad920a353f8e4bc35b7cfa9887da245
bofilio <hemzatalha@gmail.com>
2022-03-05 15:26:12 +0100

    working on explore

--- src/components/content/RestaurantList.tsx
+++ src/components/content/RestaurantList.tsx
@@ -3,13 +3,12 @@ import { ScrollView, View } from 'react-native'
 import tailwind from 'tailwind-rn'
 import { Restaurant } from './Restaurant'
 
+
 export const RestaurantList = () => {
     return (
         <ScrollView showsHorizontalScrollIndicator={false} horizontal>
             <View style={tailwind('flex flex-row px-2')}>
-                <Restaurant />
-                <Restaurant />
-                <Restaurant />
+                
             </View>
         </ScrollView>
     )
